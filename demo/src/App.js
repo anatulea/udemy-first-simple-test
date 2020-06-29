33d51e4174d83eba278a2c05d1b3c70032ef3fa7
Ana Tulea <ana@Anas-MacBook-Pro.local>
2020-06-29 15:30:51 -0700

    Added tests for decrement button

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,19 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  render(){
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      error:false,
+    };
+       // need to bind this for decrementCounter and incrementCounter, since they use
+    // this.state and this.setStat
+    this.decrementCounter = this.decrementCounter.bind(this)
+    this.incrementCounter = this.incrementCounter.bind(this)
+  }
+
+  
+  decrementCounter() {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  }
+  incrementCounter() {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ counter: this.state.counter + 1 });
+  }
+ 
+  render() {
+    console.log(this.state.counter)
+    const errorClass = this.state.error ? '' : 'hidden';
+
     return (
-      <div className="App" data-test='component-app'>
+      <div className="App" data-test="component-app">
         <h1>Learn react testing!!!</h1>
-        <h1 data-test='counter-display'> Te counter is curently: </h1>
-        <button data-test='increment-button'>Increment counter</button>
+        <h1 data-test="counter-display">
+          {" "}
+          The counter is curently:{this.state.counter}{" "}
+        </h1>
+        <div data-test="error-message" className={`error ${errorClass}`}>
+        The counter cannot go below 0
       </div>
-      
+        <button
+          data-test="increment-button"
+          onClick={this.incrementCounter}
+        >
+          Increment counter
+        </button>
+        <button
+          data-test="decrement-button"
+          onClick={this.decrementCounter}
+        >
+          Decrement counter
+        </button>
+      </div>
     );
   }
-  
 }
 
 export default App;
