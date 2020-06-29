44b27dffbe5afec729f9996c0ccf505fbe209396
Ana Tulea <ana@Anas-MacBook-Pro.local>
2020-06-28 22:54:51 -0700

    Added first tests

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Learn react testing</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App" data-test='component-app'>
+        <h1>Learn react testing!!!</h1>
+        <h1 data-test='counter-display'> Te counter is curently: </h1>
+        <button data-test='increment-button'>Increment counter</button>
+      </div>
+      
+    );
+  }
+  
 }
 
 export default App;
--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+ Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders withoutt error', () => {
+ const wrapper = shallow(<App/>);
+ const appComponent = wrapper.find("[data-test='component-app']");
+ expect(appComponent.length).toBe(1);
+ //console.log(wrapper.debug())
+ //expect(wrapper).toBeTruthy();
 });
+test('renders increment button', () => {
+  const wrapper = shallow(<App/>);
+ const button = wrapper.find("[data-test='increment-button']");
+ expect(button.length).toBe(1);
+ });
+ test('renders counter display', () => {
+  const wrapper = shallow(<App/>);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
+
